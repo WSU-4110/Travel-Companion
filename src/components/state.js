a9d12d0714104ffc3957d0fcae751db8d92c62e2
CCooley33 <hj4005@wayne.edu>
2024-02-13 23:39:18 -0500

    Committing Sidebar files

---
+++ src/components/state.js
@@ -0,0 +1,11 @@
+import { ref, computed} from 'vue'
+
+//Handling sidebarWidth based on state of collapsed
+export const collapsed = ref(false)
+export const toggleSidebar = () => (collapsed.value = !collapsed.value)
+
+export const SIDEBAR_WIDTH = 250
+export const SIDEBAR_WIDTH_COLLAPSED = 80
+export const sidebarWidth = computed(
+    () => `${collapsed.value ? SIDEBAR_WIDTH_COLLAPSED : SIDEBAR_WIDTH}px`
+)
