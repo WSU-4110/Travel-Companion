29bec95b6068b3787110969df81abe3077611e37
CCooley33 <hj4005@wayne.edu>
2024-02-19 11:20:37 -0500

    Adjusted about us page and sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -36,6 +36,7 @@ export default{
         <h5>
         <!-- Sidebar Links -->
         <SidebarLink v-if="username" to="/" icon="fas fa-home">Home</SidebarLink>
+        <SidebarLink v-if="username" to="/aboutUs" icon="fas fa-people-group">About Us</SidebarLink>
         <SidebarLink v-if="username" to="/UserLocation" icon="fas fa-map-location-dot">Map Tool</SidebarLink>
         <SidebarLink v-if="username" to="/tripManager" icon="fas fa-plane">Trip Manager</SidebarLink>
         <SidebarLink v-if="!username" to="/signIn" icon="fas fa-right-to-bracket">Sign In</SidebarLink>
