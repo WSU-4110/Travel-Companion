90e57db496d70fe080b18c6adbfdaa4a1da1133a
Michael Rago <mrago256@gmail.com>
2024-02-22 15:26:27 -0500

    Remove test function

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="home">
     <h3 v-if="username">Welcome {{ username }}!</h3>
-    <button @click="testFunc">
-      mmmm
-    </button>
   </div>
 </template>
 
@@ -12,12 +9,6 @@ export default {
   computed: {
     username() {
       return this.$store.getters.getUsername;
-    }
-  },
-  methods: {
-    testFunc() {
-      this.$store.commit('setAlertStatus', 'alert-success');
-      this.$store.commit('setAlertMessage', 'tessttttt');
     }
   }
 }
