84b820f3de8db91393e7d468d6fbaed94ddc927e
BrandonSokolowskiWSU <ga2751@wayne.edu>
2024-03-19 02:19:55 -0400

    Add weather icon and bolded characters for categories

--- src/views/Weather.vue
+++ src/views/Weather.vue
@@ -6,14 +6,15 @@
         </form>
         <div v-if="result.temp">
             <!-- Weather Display -->
-            <p>Condition: {{ weather_main }}, {{ weather_description }}</p> <!-- Display weather description -->
-            <p>Temperature: {{ convert_kelvin_to_fahrenheit(result.temp) }}°F ({{ convert_kelvin_to_celsius(result.temp) }}°C)</p> <!-- Displays temp in F and C -->
-            <p>Feels Like: {{ convert_kelvin_to_fahrenheit(result.feels_like) }}°F ({{ convert_kelvin_to_celsius(result.feels_like) }}°C)</p> <!-- Displays feels like in F and C -->
-            <p>Humidity: {{ result.humidity }}%</p> <!-- Displays air humidity -->
-            <p>Pressure: {{ result.pressure }} hPa</p> <!-- Displays air pressure in hPa -->
-            <p>Wind Speed: {{ convert_mps_to_mph(wind_speed) }} mph ({{ wind_speed }} m/s)</p> <!-- Displays wind speed in mph and m/s -->
-            <p>Wind Direction: {{ wind_direction }}°</p> <!-- Display wind direction -->
-            <p>Cloudiness: {{ clouds_percent }}%</p> <!-- Display cloud coverage -->
+            <img v-if="weather_icon" :src="weather_icon_url" alt="Weather Icon" /> <!-- Display weather icon -->
+            <p><b>Condition:</b> {{ weather_main }}, {{ weather_description }}</p> <!-- Display weather description -->
+            <p><b>Temperature:</b> {{ convert_kelvin_to_fahrenheit(result.temp) }}°F ({{ convert_kelvin_to_celsius(result.temp) }}°C)</p> <!-- Displays temp in F and C -->
+            <p><b>Feels Like:</b> {{ convert_kelvin_to_fahrenheit(result.feels_like) }}°F ({{ convert_kelvin_to_celsius(result.feels_like) }}°C)</p> <!-- Displays feels like in F and C -->
+            <p><b>Humidity:</b> {{ result.humidity }}%</p> <!-- Displays air humidity -->
+            <p><b>Pressure:</b> {{ result.pressure }} hPa</p> <!-- Displays air pressure in hPa -->
+            <p><b>Wind Speed:</b> {{ convert_mps_to_mph(wind_speed) }} mph ({{ wind_speed }} m/s)</p> <!-- Displays wind speed in mph and m/s -->
+            <p><b>Wind Direction:</b> {{ wind_direction }}°</p> <!-- Display wind direction -->
+            <p><b>Cloudiness:</b> {{ clouds_percent }}%</p> <!-- Display cloud coverage -->
             <p>Weather provided by OpenWeather</p>
         </div>
         <div v-else>
@@ -51,6 +52,7 @@
                             this.result = data.main; //typing API's data.main output to our result function
                             this.weather_description = data.weather[0].description; //typing API's data.weather[0].description output to our description function
                             this.weather_main = data.weather[0].main; //typing API's data.weather[0].main output to our weather_main function
+                            this.weather_icon = data.weather[0].icon; //typing API's weather icons
                             this.wind_speed = data.wind.speed; //typing API's data.wind.speed function to our wind_speed function
                             this.wind_direction = data.wind.deg || 0; // tying API's data.wind.deg output to our wind_direction function
                             this.clouds_percent = data.clouds.all; // typing API's data.clouds.all output to our clouds_percent function
@@ -66,6 +68,12 @@
                     }
                 },
 
-            }
+            },
+            computed: {
+                // Computed property to construct weather icon URL
+                weather_icon_url() {
+                    return `https://openweathermap.org/img/wn/${this.weather_icon}@2x.png`;
+                },
+            },
         };
 </script>
